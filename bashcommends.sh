e8fe890bf00a8c360d06d7d79f237e88a687f491
´SLTRgit config --global user.name ´SLTR <ikbenstefanz@gmail.com>
2024-05-29 15:04:43 +0200

    commit_1_29/5

--- bashcommends.sh
+++ bashcommends.sh
@@ -1,5 +1,6 @@
-        cd /farm
-        git pull
-        source venv/bin/activate
-        pip install -r requirements.txt
-        sudo systemctl restart flaskapp
+cd home/farm
+          git pull
+          source venv/bin/activate
+          pip install -r requirements.txt
+          systemctl restart flaskapp
+          systemctl status flaskapp'
