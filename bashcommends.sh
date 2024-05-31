c68f7794833f7faa7d8e2fef63d7ab4ca8381f33
SLTR34 <ikbenstefanz@gmail.com>
2024-05-31 10:51:44 +0200

    commit_2_31/5

--- bashcommends.sh
+++ bashcommends.sh
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-        cd /farm
+        cd /home/farm/
         git pull
         source venv/bin/activate
         pip install -r requirements.txt
         sudo systemctl restart flaskapp
         systemctl status flaskapp
-=======
-          cd home/farm
-          git pull
-          python3 -m venv venv
-          source venv/bin/activate
-          pip install -r requirements.txt
-          systemctl restart flaskapp
-          systemctl status flaskapp'
->>>>>>> 5d4aeae7e53c0564cd10950bc84c9caa081c4ba4
